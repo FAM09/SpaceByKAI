2a47dc05adf24e486b06d9a07c400d269cc5c5cb
FAM94 <azharif15@gmail.com>
2023-12-10 21:31:40 +0700

    Initial Commit

---
+++ MitraApps/Katalog.swift
@@ -0,0 +1,18 @@
+//
+//  Katalog.swift
+//  MitraApps
+//
+//  Created by Faiz Azhari Mursid on 12/12/23.
+//
+
+import SwiftUI
+
+struct Katalog: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Katalog()
+}
---
+++ MitraApps/MitraAppsApp.swift
@@ -0,0 +1,17 @@
+//
+//  MitraAppsApp.swift
+//  MitraApps
+//
+//  Created by Faiz Azhari Mursid on 10/12/23.
+//
+
+import SwiftUI
+
+@main
+struct MitraAppsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
